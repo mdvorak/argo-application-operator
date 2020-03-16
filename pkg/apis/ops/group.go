16ebd95e1bbf28537e04a3b23619bca5ceb70fb3
Michal Dvorak <michal@mdvorak.org>
2020-03-16 16:45:06 +0100

    api skeleton

---
+++ pkg/apis/ops/group.go
@@ -0,0 +1,6 @@
+// Package ops contains ops API versions.
+//
+// This file ensures Go source parsers acknowledge the ops package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package ops
