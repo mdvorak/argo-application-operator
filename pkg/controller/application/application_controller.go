bddb08b83a2d3ba4c4203e9b4c92c0598597709c
Michal Dvorak <michal@mdvorak.org>
2020-03-17 14:18:42 +0100

    reformatted labels for finalizer, hacked buggy Get
    added workaround method as getNoCache

--- pkg/controller/application/application_controller.go
+++ pkg/controller/application/application_controller.go
@@ -2,9 +2,11 @@ package application
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"github.com/go-logr/logr"
+	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"os"
+	"reflect"
 	"strings"
 
 	argocdv1alpha1 "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
@@ -26,11 +28,14 @@ var log = logf.Log.WithName("controller_application")
 var targetNamespace string
 
 const TargetNamespaceEnvVar = "TARGET_NAMESPACE"
+const applicationKind = "Application"
 const applicationFinalizer = "finalizer.application.ops.csas.cz"
-const ownerApiVersionLabel = "application.ops.csas.cz/owner-apiVersion"
+const ownerApiGroupLabel = "application.ops.csas.cz/owner-group"
+const ownerApiVersionLabel = "application.ops.csas.cz/owner-version"
 const ownerKindLabel = "application.ops.csas.cz/owner-kind"
 const ownerNameLabel = "application.ops.csas.cz/owner-name"
 const ownerNamespaceLabel = "application.ops.csas.cz/owner-namespace"
+const managedByLabel = "app.kubernetes.io/managed-by"
 
 // Add creates a new Application Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
@@ -45,13 +50,19 @@ func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Store targetNamespace
+	var err error
+
+	// Store targetNamespace for later use
 	if ns, ok := os.LookupEnv(TargetNamespaceEnvVar); ok && len(ns) > 0 {
 		targetNamespace = ns
 	} else {
-		// TODO fallback
-		return errors.New(TargetNamespaceEnvVar + " not set")
+		// Fallback for local development
+		targetNamespace, err = k8sutil.GetWatchNamespace()
 	}
+	if len(targetNamespace) == 0 {
+		return fmt.Errorf("%s env variable must be set", TargetNamespaceEnvVar)
+	}
+	log.Info(fmt.Sprintf("%s=%s", TargetNamespaceEnvVar, targetNamespace))
 
 	// Create a new controller
 	c, err := controller.New("application-controller", mgr, controller.Options{Reconciler: r})
@@ -67,22 +78,34 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 
 	// Watch for changes to secondary resource Application and requeue the owner Application
 	err = c.Watch(&source.Kind{Type: &argocdv1alpha1.Application{}}, &handler.EnqueueRequestsFromMapFunc{
-		ToRequests: handler.ToRequestsFunc(func(obj handler.MapObject) []reconcile.Request {
-			// TODO kind, ver, and only if matches
-			// managed-by
-			return []reconcile.Request{
-				{NamespacedName: types.NamespacedName{
-					Name:      obj.Meta.GetLabels()[ownerNameLabel],
-					Namespace: obj.Meta.GetLabels()[ownerNamespaceLabel],
-				}},
-			}
-		}),
+		ToRequests: handler.ToRequestsFunc(watchMapFunc),
 	})
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func watchMapFunc(obj handler.MapObject) []reconcile.Request {
+	apiGroup := obj.Meta.GetLabels()[ownerApiGroupLabel]
+	apiVersion := obj.Meta.GetLabels()[ownerApiVersionLabel]
+	kind := obj.Meta.GetLabels()[ownerKindLabel]
+
+	if apiGroup != opsv1alpha1.SchemeGroupVersion.Group ||
+		apiVersion != opsv1alpha1.SchemeGroupVersion.Version ||
+		kind != applicationKind {
+		// Mismatch, ignore
+		return []reconcile.Request{}
+	}
+
+	// Start reconcile
+	return []reconcile.Request{
+		{NamespacedName: types.NamespacedName{
+			Name:      obj.Meta.GetLabels()[ownerNameLabel],
+			Namespace: obj.Meta.GetLabels()[ownerNamespaceLabel],
+		}},
+	}
 }
 
 // blank assignment to verify that ReconcileApplication implements reconcile.Reconciler
@@ -122,19 +145,22 @@ func (r *ReconcileApplication) Reconcile(request reconcile.Request) (reconcile.R
 
 	// Define a new Argo Application object
 	app := newApplication(instance)
+	appLogger := reqLogger.WithValues("Application.Namespace", app.Namespace, "Application.Name", app.Name)
 
 	// Check if the instance is marked to be deleted, which is indicated by the deletion timestamp being set.
 	markedToBeDeleted := instance.GetDeletionTimestamp() != nil
 	if markedToBeDeleted {
+		appLogger.Info("Application is marked to be deleted")
+
 		if contains(instance.GetFinalizers(), applicationFinalizer) {
 			// Run finalization logic for our finalizer. If the finalization logic fails,
 			// don't remove the finalizer so that we can retry during the next reconciliation.
-			if err := r.finalizeApplication(reqLogger, instance, app); err != nil {
+			if err := r.finalizeApplication(appLogger, instance, app); err != nil {
 				return reconcile.Result{}, err
 			}
 
 			// Remove the finalizer. Once all finalizers have been removed, the object will be deleted.
-			if err := r.removeFinalizer(reqLogger, instance); err != nil {
+			if err := r.removeFinalizer(appLogger, instance); err != nil {
 				return reconcile.Result{}, err
 			}
 		}
@@ -143,16 +169,17 @@ func (r *ReconcileApplication) Reconcile(request reconcile.Request) (reconcile.R
 
 	// Add finalizer for this CR
 	if !contains(instance.GetFinalizers(), applicationFinalizer) {
-		if err := r.addFinalizer(reqLogger, instance); err != nil {
+		appLogger.Info("Adding finalizer")
+		if err := r.addFinalizer(appLogger, instance); err != nil {
 			return reconcile.Result{}, err
 		}
 	}
 
 	// Check if this Application already exists
 	found := &argocdv1alpha1.Application{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: app.Name, Namespace: app.Namespace}, found)
+	err = getNoCache(r.client, context.TODO(), types.NamespacedName{Name: app.Name, Namespace: app.Namespace}, found)
 	if err != nil && k8serrors.IsNotFound(err) {
-		reqLogger.Info("Creating a new Application", "Application.Namespace", app.Namespace, "Application.Name", app.Name)
+		appLogger.Info("Creating a new Application")
 		err = r.client.Create(context.TODO(), app)
 		if err != nil {
 			return reconcile.Result{}, err
@@ -164,55 +191,80 @@ func (r *ReconcileApplication) Reconcile(request reconcile.Request) (reconcile.R
 		return reconcile.Result{}, err
 	}
 
+	// Application exists, update
+	isChanged := false
+	for label, value := range app.Labels {
+		if found.Labels[label] != value {
+			found.Labels[label] = value
+			isChanged = true
+		}
+	}
+	if !reflect.DeepEqual(found.Spec, app.Spec) {
+		found.Spec = app.Spec
+		isChanged = true
+	}
+
+	// Change detected
+	if isChanged {
+		appLogger.Info("Updating Application")
+		err = r.client.Update(context.TODO(), found)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+
 	// Application already exists - don't requeue
-	reqLogger.Info("Skip reconcile: Application already exists", "Application.Namespace", found.Namespace, "Application.Name", found.Name)
+	appLogger.Info("Reconcile finished")
 	return reconcile.Result{}, nil
 }
 
-func (r *ReconcileApplication) addFinalizer(reqLogger logr.Logger, instance *opsv1alpha1.Application) error {
-	reqLogger.Info("Adding Finalizer")
+func (r *ReconcileApplication) addFinalizer(logger logr.Logger, instance *opsv1alpha1.Application) error {
+	logger.Info("Adding Finalizer")
 	instance.SetFinalizers(append(instance.GetFinalizers(), applicationFinalizer))
 
 	// Update CR
 	err := r.client.Update(context.TODO(), instance)
 	if err != nil {
-		reqLogger.Error(err, "Failed to update Application with finalizer")
+		logger.Error(err, "Failed to update Application with finalizer")
 		return err
 	}
 	return nil
 }
 
-func (r *ReconcileApplication) removeFinalizer(reqLogger logr.Logger, instance *opsv1alpha1.Application) error {
+func (r *ReconcileApplication) removeFinalizer(logger logr.Logger, instance *opsv1alpha1.Application) error {
 	instance.SetFinalizers(remove(instance.GetFinalizers(), applicationFinalizer))
 	err := r.client.Update(context.TODO(), instance)
 	if err != nil {
-		reqLogger.Error(err, "Failed to update Application without finalizer")
+		logger.Error(err, "Failed to update Application without finalizer")
 		return err
 	}
 	return nil
 }
 
-func (r *ReconcileApplication) finalizeApplication(reqLogger logr.Logger, instance *opsv1alpha1.Application, app *argocdv1alpha1.Application) error {
-	reqLogger.Info("Running finalizer", "Application.Namespace", instance.Namespace, "Application.Name", instance.Name)
+func (r *ReconcileApplication) finalizeApplication(logger logr.Logger, instance *opsv1alpha1.Application, app *argocdv1alpha1.Application) error {
+	logger.Info("Running finalizer")
 
 	// Check if this Application exists
 	found := &argocdv1alpha1.Application{}
-	err := r.client.Get(context.TODO(), types.NamespacedName{Name: app.Name, Namespace: app.Namespace}, found)
+	err := getNoCache(r.client, context.TODO(), types.NamespacedName{Name: app.Name, Namespace: app.Namespace}, found)
 	if err != nil {
 		// If there was error but it wasn't NotFound, propagate the error
 		if k8serrors.IsNotFound(err) {
 			// Already deleted, nothing to do
+			logger.Info("Application already deleted")
 			return nil
 		}
 		return err
 	}
 
 	// Delete
+	logger.Info("Deleting application")
 	err = r.client.Delete(context.TODO(), found)
 	if err != nil {
-		reqLogger.Error(err, "Failed to delete Application")
+		logger.Error(err, "Failed to delete Application")
 		return err
 	}
+
 	return nil
 }
 
@@ -226,24 +278,38 @@ func newApplication(cr *opsv1alpha1.Application) *argocdv1alpha1.Application {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: targetNamespace,
-			Labels: map[string]string{
-				ownerApiVersionLabel: opsv1alpha1.SchemeGroupVersion.String(),
-				ownerKindLabel:       "Application",
-				ownerNamespaceLabel:  cr.Namespace,
-				ownerNameLabel:       cr.Name,
-			},
+			Labels:    applicationLabels(cr),
 		},
-		Spec: argocdv1alpha1.ApplicationSpec{
-			Source: cr.Spec.Source,
-			Destination: argocdv1alpha1.ApplicationDestination{
-				Server:    "https://kubernetes.default.svc", // TODO
-				Namespace: cr.Namespace,
-			},
-			Project:              cr.Namespace,
-			SyncPolicy:           cr.Spec.SyncPolicy,
-			IgnoreDifferences:    cr.Spec.IgnoreDifferences,
-			Info:                 cr.Spec.Info,
-			RevisionHistoryLimit: nil,
+		Spec: newApplicationSpec(cr),
+	}
+}
+
+func applicationLabels(cr *opsv1alpha1.Application) map[string]string {
+	// Get name, ignore error
+	operatorName, _ := k8sutil.GetOperatorName()
+
+	// Return required labels
+	return map[string]string{
+		ownerApiGroupLabel:   opsv1alpha1.SchemeGroupVersion.Group,
+		ownerApiVersionLabel: opsv1alpha1.SchemeGroupVersion.Version,
+		ownerKindLabel:       applicationKind,
+		ownerNamespaceLabel:  cr.Namespace,
+		ownerNameLabel:       cr.Name,
+		managedByLabel:       operatorName,
+	}
+}
+
+func newApplicationSpec(cr *opsv1alpha1.Application) argocdv1alpha1.ApplicationSpec {
+	return argocdv1alpha1.ApplicationSpec{
+		Source: cr.Spec.Source,
+		Destination: argocdv1alpha1.ApplicationDestination{
+			Server:    "https://kubernetes.default.svc", // TODO
+			Namespace: cr.Namespace,
 		},
+		Project:              cr.Namespace,
+		SyncPolicy:           cr.Spec.SyncPolicy,
+		IgnoreDifferences:    cr.Spec.IgnoreDifferences,
+		Info:                 cr.Spec.Info,
+		RevisionHistoryLimit: nil,
 	}
 }
