fd433d5ae44dff3374954c092a7872f035ea2829
Michal Dvorak <michal@mdvorak.org>
2020-03-19 13:08:41 +0100

    deleted build.sh

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-go build -o ./build/_output/bin/argo-application-operator -gcflags all=-trimpath=/home/cen38289/Workspace -asmflags all=-trimpath=/home/cen38289/Workspace github.com/mdvorak/argo-application-operator/cmd/manager
